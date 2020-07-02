3e031235e8c33ef5fdc66093ba58aab93ac8e560
p0i0k0i <yuta2013.15@gmail.com>
2020-07-02 19:52:24 +0900

    add many file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  root 'application#hello'
-
   get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
-
+  
+  
   get "/" => "home#top"
   get "about" => "home#about"
-
-
 end
